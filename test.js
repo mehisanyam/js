a35ccc18eb16b57817a1b2bf576de7ab179c0122
sanyam <mehi.sanyam@gmail.com>
2024-08-07 19:02:22 +0530

    js-new

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
 console.log("name")
+const account=1234556
+var id=235
+let gk='wq'
+sk=23
+let fk=null
+
+gk='3'
+// account='asa'
+sk='3r'
+id=23456789
+
+console.table([account,id,gk,sk,fk])
+console.log(typeof null); //string
+
+ 
+
+
